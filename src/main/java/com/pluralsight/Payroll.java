74b66b797b043bdc8ee17a7ad11a2eb0bfc220ed
Sthealth <AW@example.com>
2025-10-07 11:24:21 -0400

    add updated payroll function, read file, and employee class

---
+++ src/main/java/com/pluralsight/Payroll.java
@@ -0,0 +1,44 @@
+package com.pluralsight;
+import java.io.*;
+import java.util.Scanner;
+
+public class Payroll {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter the name of the file employee file to process: ");
+        String employeeFile = myScanner.nextLine();
+
+        System.out.println("Enter the name of the payroll file to create: ");
+        String payrollFile = myScanner.nextLine();
+
+        try {
+            BufferedReader myReader = new BufferedReader(new FileReader(employeeFile));
+            BufferedWriter myWriter = new BufferedWriter(new FileWriter(payrollFile));
+
+            String line;
+            String text;
+            while ((line = myReader.readLine()) != null) {
+                String[] tokens = line.split("\\|");
+                int id = Integer.parseInt(tokens[0]);
+                String name = tokens[1];
+                double hoursWorked = Double.parseDouble(tokens[2]);
+                double payRate = Double.parseDouble(tokens[3]);
+
+                Employee employee = new Employee(id, name, hoursWorked, payRate);
+
+                System.out.printf("Employee ID: %d, Name: %s, Gross Pay: $%.2f%n",
+                        employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+
+                text = String.format("%d | %s | %.2f\n",
+                        employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+                myWriter.write(text);
+            }
+            myScanner.close();
+            myWriter.close();
+            myReader.close();
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + employeeFile);
+        }
+    }
+}
